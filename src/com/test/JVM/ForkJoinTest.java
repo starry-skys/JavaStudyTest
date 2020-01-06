1d12060b08b3219b95bf8333d5390b48246284ad
zhangwanbei <zhang.wanbei@pufubao.net>
2020-01-06 17:34:48 +0800

    compare test

--- src/com/test/JVM/ForkJoinTest.java
+++ src/com/test/JVM/ForkJoinTest.java
@@ -1,24 +1,45 @@
 package com.test.JVM;
 
 import java.util.concurrent.ForkJoinPool;
+import java.util.stream.LongStream;
 
 public class ForkJoinTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+        Long start = 0L;
+        Long end = 10_0000_0000L;
 
+        testForkJoin(start,end);
     }
 
-    public static void testwork(){
+    public static void testSum(Long start,Long end){
         long l = System.currentTimeMillis();
 
-        Long start = 0L;
-        Long end = 1_0000_0000L;
+        Long sum = 0L;
+        for (Long i = start; i <= end ; i++) {
+            sum += i;
+        }
+
+        long l1 = System.currentTimeMillis();
+        System.out.println("结果:"+sum+",耗时:"+(l1-l));
+    }
+
+    public static void testForkJoin(Long start,Long end) throws Exception{
+        long l = System.currentTimeMillis();
 
         ForkJoinPool forkJoinPool = new ForkJoinPool();
-        ForkJoinWork work = new ForkJoinWork(start,end);
-        Long invoke = forkJoinPool.invoke(work);
+        ForkJoinWork task = new ForkJoinWork(start,end);
+        Long invoke = forkJoinPool.invoke(task);
 
         long l1 = System.currentTimeMillis();
         System.out.println("结果:"+invoke+",耗时:"+(l1-l));
+    }
 
+    public static void testStream(Long start,Long end){
+        long l = System.currentTimeMillis();
+
+        long reduce = LongStream.rangeClosed(start, end).parallel().reduce(0, (x, y) -> x + y);
+
+        long l1 = System.currentTimeMillis();
+        System.out.println("结果:"+reduce+",耗时:"+(l1-l));
     }
 }
--- src/com/test/JVM/ForkJoinWork.java
+++ src/com/test/JVM/ForkJoinWork.java
@@ -8,7 +8,7 @@ public class ForkJoinWork extends RecursiveTask<Long> {
     private Long end;
 
     //临界点
-    private static Long critical = 10000L;
+    private static final Long THRESHOLD = 10000L;
 
     public ForkJoinWork(Long start, Long end) {
         this.start = start;
@@ -17,23 +17,24 @@ public class ForkJoinWork extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        Long len = start - end;
-        if(len < critical){
+        Long len = end - start;
+        if(len < THRESHOLD){
             Long sum = 0L;
-            for (Long i = start; i < end; i++) {
+            for (Long i = start; i <= end; i++) {
                 sum += i;
             }
+            System.out.println("普通累加");
             return sum;
         }else{
+            System.out.println("forkjoin累加");
             //拆分为两个任务
-            Long mid = start + end /2;
-            ForkJoinWork work1 = new ForkJoinWork(start,mid);
-            work1.fork(); //分支一
-            ForkJoinWork work2 = new ForkJoinWork(start,mid);
-            work2.fork(); //分支二
+            Long mid = (start + end) /2;
+            ForkJoinWork task1 = new ForkJoinWork(start,mid);
+            ForkJoinWork task2 = new ForkJoinWork(mid+1,end);
 
+            invokeAll(task1,task2);
             //合并计算
-            return work1.join() + work2.join();
+            return task1.join() + task2.join();
         }
 
     }
